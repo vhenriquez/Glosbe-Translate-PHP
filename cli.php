f9300bcd04830222582c5debc547f534f2b374d9
ms <schmiddim@gmx.at>
2015-09-24 08:37:47 +0200

    example

--- cli.php
+++ cli.php
@@ -12,9 +12,27 @@ require_once dirname(__FILE__) . DIRECTORY_SEPARATOR . 'vendor/autoload.php';
 
 $glosbe = new \Translate\GlosbeTranslate("ita", "de");
 
-$glosbe->translate('ciao');
+$glosbe->translate('come');
 
-var_dump( $glosbe->translationsOnly());
+#var_dump( $glosbe->translationsOnly());
+
+
+$csv =  dirname(__FILE__) . DIRECTORY_SEPARATOR . 'assets' . DIRECTORY_SEPARATOR . 'words.csv';
+$words = array();
+$lines =file_get_contents($csv);
+$translations = array();
+foreach(explode("\n",$lines) as $line) {
+	$words []= explode(';', $line)[0];
+
+}
+
+
+foreach($words as $word) {
+	var_dump($glosbe->translate($word));
+}
+
+
+
 /*print_r(
 json_decode($glosbe->translate('ciao'))
 
